597086ecefe4fed76afb2018666f1d7e0f363bc5
Hiroki Chiyoda <contact@arafipro.com>
2024-08-11 22:25:46 +0900

    不要なコードを削除

---
+++ blog/2024-08-11-blank-post.md
@@ -0,0 +1,8 @@
+---
+slug: blank-post
+title: Blank Post
+---
+
+This is a blank post.
+
+<!--truncate-->
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,9 +38,6 @@ export default function Home(): JSX.Element {
     >
       <HomepageHeader />
       <main>
-        <p className="flex justify-center text-7xl text-red-500">
-          Tailwind CSS test
-        </p>
         <HomepageFeatures />
       </main>
     </Layout>
